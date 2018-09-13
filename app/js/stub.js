acbad80c1e1643b5741be967deb93009c0d7c0ec
Pererva <pererva.kostya@outlook.com>
2018-09-14 01:50:53 +0300

    media

--- app/js/stub.js
+++ app/js/stub.js
@@ -12,6 +12,8 @@ var slider = new Swiper('.swiper-container', {
     speed: 400,
     spaceBetween: 92,
     slidesPerView: 'auto',
+    threshold: 20,
+    preventClicks: true,
     scrollbar: {
         el: '.swiper-scrollbar',
         draggable: true,
@@ -19,8 +21,25 @@ var slider = new Swiper('.swiper-container', {
         dragSize: '330'
     },
     breakpoints: {
+        1440: {
+            scrollbar: {
+                dragSize: '200'
+            }
+        },
+        1366: {
+            spaceBetween: 60,
+            scrollbar: {
+                dragSize: '160'
+            }
+        },
         1023: {
             spaceBetween: 20
+        },
+        767: {
+            spaceBetween: 10,
+            scrollbar: {
+                dragSize: '60'
+            }
         }
     }
 });
