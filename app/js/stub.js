89f573126b1c89ffe9afe88d8ab1f947d8f0b47d
PerervaKostya <pererva.kostya@outlook.com>
2018-09-27 18:32:47 +0300

    markup slider and edit page

--- app/js/stub.js
+++ app/js/stub.js
@@ -1,24 +1,73 @@
-var element = document.getElementById('animation');
+if(window.innerWidth >= 768) {
+    var element = document.getElementById('animation');
 
-lottie.loadAnimation({
-  container: element,
-  renderer: 'svg',
-  loop: false,
-  autoplay: true,
-  path: 'js/data4.json'
-});
+    var logo = lottie.loadAnimation({
+        container: element,
+        renderer: 'svg',
+        loop: false,
+        path: 'js/data4.json'
+    });
+
+    var btnNextSlide = document.querySelector('[data="next-slide"]');
+
+    var swiper = new Swiper('.stub-wrap', {
+        speed: 600,
+        direction: 'vertical',
+        slidesPerView: 1,
+        freeMode: false,
+        resistanceRatio: 0,
+        navigation: {
+            nextEl: btnNextSlide
+        },
+        mousewheel: {
+            releaseOnEdges: true,
+            invert: false
+        },
+        preventClicks: true,
+        scrollbar: {
+            el: '.page-scrollbar'
+        },
+        breakpoints: {
+            1024: {
+                scrollbar: false
+            },
+            767: {
+                spaceBetween: 10
+            }
+        }
+
+    });
+
+    document.querySelector('.slider-projects').addEventListener('mouseenter', function () {
+        swiper.mousewheel.disable();
+    });
+
+    document.querySelector('.slider-projects').addEventListener('mouseleave', function () {
+        swiper.mousewheel.enable();
+    });
+
+    swiper.on('slideChange', function () {
+        var cont = document.querySelector('.b24-widget-button-position-bottom-right');
+        if (swiper.activeIndex === 1) {
+            cont.style.bottom = "130px";
+        } else {
+            cont.style.bottom = "30px";
+        }
+    });
+
+    swiper.on('resize', function () {
+        this.init();
+    });
+
+
+}
 
 var slider = new Swiper('.slider-projects', {
-    speed: 1000,
+    speed: 700,
     spaceBetween: 92,
-    slidesPerView: 'auto',
+    slidesPerView: 3,
     preventClicks: true,
-    observer: true,
-    mousewheel: {
-        sensitivity: 1,
-        // forceToAxis: false,
-    },
-    // freeMode: true,
+    mousewheel: true,
     scrollbar: {
         el: '.swiper-scrollbar',
         draggable: true,
@@ -37,165 +86,40 @@ var slider = new Swiper('.slider-projects', {
                 dragSize: '160'
             }
         },
-        1280: {
-            mousewheel: false
+        1024: {
+            scrollbar: {
+                dragSize: '140'
+            }
         },
         1023: {
-            spaceBetween: 20
+            spaceBetween: 20,
+            scrollbar: {
+                dragSize: '120'
+            }
+        },
+        768: {
+            slidesPerView: 2,
+            scrollbar: {
+                dragSize: '120'
+            }
         },
         767: {
+            slidesPerView: "auto",
             spaceBetween: 10,
             scrollbar: {
                 dragSize: '60'
+            }
+        },
+        414: {
+            slidesPerView: "auto",
+            spaceBetween: 10,
+            scrollbar: {
+                dragSize: '40'
             }
         }
     }
 });
 
-var swiper = new Swiper('.stub-wrap', {
-    speed: 400,
-    direction: 'vertical',
-    slidesPerView: 1,
-    freeMode: false,
-    resistance: false,
-    mousewheel: true,
-    observer: true,
-    scrollbar: {
-        el: '.page-scrollbar',
-    },
+slider.on('resize', function () {
+    this.init();
 });
-
-// $(document).ready(function () {
-//     var scroll = true;
-//     var sliderProjectsTop = $('#slider-projects').offset().top;
-//
-//     // function stopWheel(e){
-//     //     if(!e){ /* IE7, IE8, Chrome, Safari */
-//     //         e = window.event;
-//     //     }
-//     //     if(e.preventDefault) { /* Chrome, Safari, Firefox */
-//     //         e.preventDefault();
-//     //     }
-//     //     e.returnValue = false; /* IE7, IE8 */
-//     // }
-//     // $(document).on("mousewheel",function () {
-//     //
-//     //     if ($(this).scrollTop() > 150 && scroll) {
-//     //         // $(document).bind('mousewheel',function(){
-//     //         //     stopWheel();
-//     //         // });
-//     //         $('body,html').animate({scrollTop: sliderProjectsTop}, 600);
-//     //         // setTimeout(function () {
-//     //         //     $(document).unbind('mousewheel');
-//     //         // },1000);
-//     //         scroll = false;
-//     //     }
-//     //
-//     //     if ($(this).scrollTop() < 150 && !scroll) {
-//     //         scroll = true;
-//     //     }
-//     // });
-//
-//     // var ScrollBar = function(obj) {
-//     //     this.scr_Box = document.getElementById(obj.box);
-//     //     this.scr_Block = document.getElementById(obj.block);
-//     //     this.scr_Txt = document.getElementById(obj.txt);
-//     //     this.speed = obj.speed || 10;
-//     //     var _this = this;
-//     //     this.scale = this.scr_Box.offsetHeight - this.scr_Block.offsetHeight;
-//     //     this.l = 0;
-//     //     this.scr_Block.onmousedown = function(e) {
-//     //         _this.down(e);
-//     //
-//     //         document.onmousemove = function(e) {
-//     //             _this.move(e);
-//     //         }
-//     //
-//     //         document.onmouseup = function() {
-//     //             var than = this;
-//     //             _this.up(than);
-//     //         }
-//     //
-//     //         if (_this.scr_Block.setCapture) {
-//     //             _this.scr_Block.setCapture();
-//     //         }
-//     //         return false;
-//     //     }
-//     //
-//     //     this.scr_Box.onmousewheel = function(e) {
-//     //         _this.wheel(e);
-//     //         return false;
-//     //     }
-//     //     if (this.scr_Box.addEventListener && typeof DOMMouseScroll) {
-//     //         this.scr_Box.addEventListener("DOMMouseScroll", function(e) {
-//     //             _this.wheel(e);
-//     //             e.preventDefault();
-//     //         }, false);
-//     //     }
-//     //
-//     // };
-//     //
-//     // ScrollBar.prototype.wheel = function(e) {
-//     //     var e = e || event;
-//     //     var eve = e.wheelDelta || e.detail;
-//     //
-//     //     if (e.wheelDelta) {
-//     //         if (eve < 0) {
-//     //             this.l += this.speed;
-//     //         } else {
-//     //             this.l -= this.speed;
-//     //         }
-//     //     } else {
-//     //         if (eve > 0) {
-//     //             this.l += this.speed;
-//     //         } else {
-//     //             this.l -= this.speed;
-//     //         }
-//     //     }
-//     //     this.value();
-//     // };
-//     //
-//     // ScrollBar.prototype.down = function(e) {
-//     //     var e = e || event;
-//     //     poY = e.clientY - document.body.scrollTop - this.scr_Block.offsetTop;
-//     // };
-//     //
-//     // ScrollBar.prototype.move = function(e) {
-//     //     var e = e || event;
-//     //     this.l = e.clientY - poY;
-//     //     this.value();
-//     //
-//     // };
-//     // ScrollBar.prototype.value = function() {
-//     //     if (this.l < 0) {
-//     //         this.l = 0;
-//     //     } else if (this.l > this.scale) {
-//     //         this.l = this.scale;
-//     //     }
-//     //     this.scr_Block.style.top = this.l + 'px';
-//     //     this.scr_Txt.style.top = -(this.l / this.scale) * (this.scr_Txt.offsetHeight - this.scr_Box.offsetHeight) + 'px';
-//     // };
-//     //
-//     // ScrollBar.prototype.up = function(than) {
-//     //     than.onmousemove = null;
-//     //     than.onmouseup = null;
-//     //     if (than.releaseCapture) {
-//     //         than.releaseCapture();
-//     //     }
-//     // };
-//
-// //************************************
-// //***********前台调用参数***************
-// //     new ScrollBar({
-// //         box: 'scroll-name1', //外框ID
-// //         block: 'scroll-block1', //滚动条ID
-// //         txt: 'scroll-txt1', //文本框ID
-// //         speed: 20 //滚动距离，数值越大越多（设置默认注释掉即可）
-// //     });
-//
-//     // new ScrollBar({
-//     //     box: 'scroll-name1', //外框ID
-//     //     block: 'scroll-block1', //滚动条ID
-//     //     txt: 'scroll-txt1', //文本框ID
-//     // });
-// });
